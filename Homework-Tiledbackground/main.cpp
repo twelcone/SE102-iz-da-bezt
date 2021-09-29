340ec6e9f9919e188c5f6b5601e4aeee78b54288
Đăng Vũ Quốc Minh <19520448@ms.uit.edu.vn>
2021-09-29 19:10:53 +0700

    Finish (I think so), + changing the Main Window Title

--- Homework-Tiledbackground/main.cpp
+++ Homework-Tiledbackground/main.cpp
@@ -22,7 +22,7 @@
 
 
 #define WINDOW_CLASS_NAME L"SampleWindow"
-#define MAIN_WINDOW_TITLE L"02 - Sprite animation"
+#define MAIN_WINDOW_TITLE L"Homework - Tiled Background"
 #define WINDOW_ICON_PATH L"mario.ico"
 
 #define BACKGROUND_COLOR D3DXCOLOR(200.0f/255, 200.0f/255, 255.0f/255,0.0f)
